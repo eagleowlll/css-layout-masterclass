b325b66b11a878079d8760108b76aaeb403347c3
eagleowlll <1894citizenwon@gmail.com>
2025-03-20 17:25:56 +0900

    Update baekjoon.js

--- baekjoon.js
+++ baekjoon.js
@@ -1,7 +1,26 @@
-const str = "Wrong Answer";
-const strdown = str.toLowerCase();
-console.log(strdown);
+// const str = "Wrong Answer";
+// const strdown = str.toLowerCase();
+// console.log(strdown);
 
-const stri = "Wrong Answer";
-const striup = stri.toUpperCase();
-console.log(striup);
+// const stri = "Wrong Answer";
+// const striup = stri.toUpperCase();
+// console.log(striup);
+
+function swapcase(str) {
+  let swapped = "";
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (char === char.toUpperCase()) {
+      swapped += char.toLowerCase();
+    } else {
+      swapped += char.toUpperCase();
+    }
+  }
+  return swapped;
+}
+
+const str = "CiTy";
+const swappedstr = swapcase(str);
+console.log(swappedstr);
